419268547a85a6dc4a401253c0752970d71a9d02
Yannick Huard <huard.yan@gmail.com>
2021-12-19 19:43:48 +0100

    Create first route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,10 @@
+import { Link } from "remix";
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Hello, World!</h1>
+      <Link to="/posts">Posts</Link>
     </div>
   );
 }
