1504880ab4dbeb323a3ebeabfc3acc3aaf641b43
Yannick Huard <huard.yan@gmail.com>
2021-12-19 20:18:33 +0100

    Remove node prefix

--- app/post.ts
+++ app/post.ts
@@ -1,5 +1,5 @@
-import path from "node:path";
-import fs from "node:fs/promises";
+import path from "path";
+import fs from "fs/promises";
 import parseFrontMatter from "front-matter";
 import invariant from "tiny-invariant";
 import { marked } from "marked";
